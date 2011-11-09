ca0b8ad33c60d7b482102d183532e80561d95af2
marcus <marcus@exoticmatter.com>
2011-11-09 13:42:29 +0100

    added motion-blur, as well as per-particle support

--- user-ops/Particle-Rib-Write.h
+++ user-ops/Particle-Rib-Write.h
@@ -66,20 +66,69 @@ public:
         if(!body->name().listed_in(bodyNameList))
             return;
 
-        // grab the particle shape/channels
-        const Nb::ParticleShape& particle = body->constParticleShape();
-        const Nb::BlockArray3f& xBlocks = particle.constBlocks3f("position");
+        const bool motionBlur=
+            (param1e("Motion Blur")->eval(tb)=="On");
+        std::vector<Nb::String> motionSamplesAscii =
+            param1s("Motion Samples")->eval(tb).to_vector();
+        if(motionBlur && motionSamplesAscii.size()<2)
+            NB_THROW("Must specify two or more motion samples for motion-blur");
+        em::array1f motionSamples(motionSamplesAscii.size());
+        for(int i=0; i<motionSamplesAscii.size(); ++i)
+            motionSamples(i)=atof(motionSamplesAscii[i].c_str());
 
-        // grab the required params
+        // grab the tile-layout
+        const Nb::TileLayout& layout = 
+            body->constLayout();
+
+        // grab the particle shape/channels
+        const Nb::ParticleShape& particle = 
+            body->constParticleShape();
+        const Nb::FieldShape* field = 
+            body->queryConstFieldShape();
+        const Nb::BlockArray3f& xBlocks = 
+            particle.constBlocks3f("position");
+        
+        // velocity for motion-blur
+        const Nb::BlockArray3f* uBlocks = 
+            particle.queryConstBlocks3f("velocity");        
+        const Nb::Field1f* u = field ? 
+            field->queryConstField3f("velocity",0) : 0;
+        const Nb::Field1f* v = field ? 
+            field->queryConstField3f("velocity",1) : 0;
+        const Nb::Field1f* w = field ? 
+            field->queryConstField3f("velocity",2) : 0;
+
+        // velocity source
+        const bool pvelSrc = 
+            (param1e("Velocity Source")->eval(tb) == "Particle.velocity");
+
+        if(motionBlur) {
+            if(pvelSrc && !uBlocks)
+                NB_THROW("Motion-blur setting requires Particle.velocity");
+            if(!pvelSrc && (!u || !v || !w))
+                NB_THROW("Motion-blur setting requires Field.velocity");
+        }
+       
+        // per-particle radius
+        const Nb::BlockArray1f* rBlocks = 
+            particle.queryConstBlocks1f(
+                param1s("Per-Particle Radius Channel")->eval(tb)
+                );
         const float constantRadius = 
             param1f("Constant Radius")->eval(tb);
+        const bool ppRadius =
+            (param1e("Per-Particle Radius")->eval(tb)=="On");
+        if(ppRadius && !rBlocks)
+            NB_THROW("Per-Particle Radius channel '" << 
+                     param1s("Per-Particle Radius Channel")->eval(tb) << "' " <<
+                     "not found");
 
         // 10 MB write buffer
         const int bufSize = 10000000;
         const int pad = 1000;
         char*     buf = new char[bufSize];
 
-        // write RIB
+        // setup file paths etc
 
         const Nb::String filePath =
             Nb::fullFilename(Ng::projectPath(),
@@ -90,6 +139,8 @@ public:
                 filePath,sequenceName,tb.frame,tb.timestep,
                 param1i("Frame Padding")->eval(tb)
                 );
+
+        // write RIB archive
 
         const bool binaryRib=
             (param1e("RIB Format")->eval(tb)=="Binary (Compressed)");
@@ -110,19 +161,64 @@ public:
             RiOption( "rib", "format", (RtPointer)format, RI_NULL );
         }
 
-        RtToken tokens[2] = { "P", "constantwidth" };
-      
+        const char* width = ppRadius ? "width" : "constantwidth";        
+        RtToken tokens[2] = { "P", width };
+
         // write points
         const int bcount=xBlocks.block_count();
         for(int b=0; b<bcount; ++b) {
             const Nb::Block3f& xb = xBlocks(b);
-            if(xb.size()==0) 
+            const int pcount = xb.size();
+            if(pcount==0) 
                 continue;
-            RtPointer data[2] = { (RtPointer)xb.data(), 
-                                  (RtPointer)&constantRadius };
-            RiPointsV(xb.size(), 2, tokens, data);
+            if(!motionBlur) {
+                RtPointer data[2] = { (RtPointer)xb.data(), 
+                                      ppRadius ? 
+                                      (RtPointer)(*rBlocks)(b).data() :
+                                      (RtPointer)&constantRadius };
+                RiPointsV(pcount, 2, tokens, data);                
+            } else {
+                em::array1vec3f xt(pcount);
+                RiMotionBeginV(motionSamples.size(),
+                               motionSamples.data);
+                for(int s=0; s<motionSamples.size(); ++s) {
+                    if(pvelSrc) { // Particle.velocity
+                        const float dt = (float)(tb.frame_dt*motionSamples(s));
+                        const Nb::Block3f& ub = (*uBlocks)(b);
+#pragma omp parallel for
+                        for(int p=0; p<pcount; ++p)
+                            xt(p) = xb(p) + ub(p)*dt;
+                    } else { // Field.velocity
+#pragma omp parallel for schedule(dynamic)
+                        for(int p=0; p<pcount; ++p) {
+                            Nb::Vec3f& xp = xt(p);
+                            float dt;
+                            if(s==0) {
+                                dt = (float)(tb.frame_dt*motionSamples(s));
+                                xp = xb(p);
+                            } else {
+                                dt = (float)(tb.frame_dt*(motionSamples(s)-
+                                                          motionSamples(s-1)));
+                            }                            
+                            const float ux = 
+                                Nb::sampleFieldCubic1f(xp,layout,*u);
+                            const float vx = 
+                                Nb::sampleFieldCubic1f(xp,layout,*v);
+                            const float wx = 
+                                Nb::sampleFieldCubic1f(xp,layout,*w);
+                            xp += Nb::Vec3f(ux,vx,wx)*dt;
+                        }                        
+                    }
+                    RtPointer data[2] = { (RtPointer)xt.data, 
+                                          ppRadius ? 
+                                          (RtPointer)(*rBlocks)(b).data() :
+                                          (RtPointer)&constantRadius };
+                    RiPointsV(pcount, 2, tokens, data);                
+                }
+                RiMotionEnd();
+            }
         }
-        
+
         RiEnd();
 
         NB_INFO("Wrote " << particle.size() << " particles to '" << fileName 
